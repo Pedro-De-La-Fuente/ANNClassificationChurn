2f2d3d2acf97bb35f07e919aec55f2c1e3b6cc52
Pedro-De-La-Fuente <pedro.delafuente@ibm.com>
2025-09-22 09:47:39 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import tensorflow as tf
-from sklearn.preprocessing import StandardScaler, LabelEconder, OneHotEncoder
+from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
 import pandas as pd
 import pickle
 
